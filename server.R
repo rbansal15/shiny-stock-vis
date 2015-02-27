d1023b34748118bb29b91d4f06f9c7d8cb44780f
Rahul Bansal <rbansal@Rahuls-MBP.home>
2015-02-27 00:19:13 -0500

    adding shiny files

---
+++ server.R
@@ -0,0 +1,21 @@
+# server.R
+
+library(quantmod)
+source("helpers.R")
+
+shinyServer(function(input, output) {
+  dataInput <- reactive({
+    getSymbols(input$symb, src = "yahoo", 
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+  })
+  output$plot <- renderPlot({
+    data <- dataInput()
+    if(input$adjust) data <- adjust(dataInput())
+    
+    chartSeries(dataInput(), theme = chartTheme("white"), 
+      type = "line", log.scale = input$log, TA = NULL)
+  })
+  
+})
